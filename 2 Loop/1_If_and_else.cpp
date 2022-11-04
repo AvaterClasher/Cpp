012f2bb2ae9dac8a022a5cdc7af0e717c37b9d58
Soumyadip Moni <116944847+AvaterClasher@users.noreply.github.com>
2022-11-05 01:42:49 +0530

    Delete 2 Loop directory

--- 2 Loop/1_If_and_else.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int num=10;
-    if(num % 2==0){
-        cout<<"It is divisible by 2";
-    }
-    return 0;
-}
--- 2 Loop/5_Infinite_For_Loop.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int i = 0;
-    while(i<=10){
-        cout<<i<<"\n";
-        i++;
-    }
-}
